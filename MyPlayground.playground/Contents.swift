400865aac063331a18704210605e91a08bc807b0
West Kraemer <WestKraemer@gmail.com>
2016-10-21 22:05:17 -0400

    initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var message1 = "Hello Swift! How can I get started?"
+
+var message2 = "Stop talking and code."
+
+
