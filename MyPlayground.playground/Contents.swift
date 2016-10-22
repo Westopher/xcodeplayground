e2b3f5c5c6125fd39b953e68595e68301ba5c8fd
West Kraemer <WestKraemer@gmail.com>
2016-10-21 22:11:00 -0400

    added conditional

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,6 +6,17 @@ var str = "Hello, playground"
 
 var message1 = "Hello Swift! How can I get started?"
 
-var message2 = "Stop talking and code."
+var message2 = "Stop talking and code..."
+
+message1.uppercased()
+
+message2.lowercased() + " damn, don't push me"
+
+if message1 == message2 {
+    print("same message")
+}
+    else {
+    print ("different message")
+}
 
 
